df34c4f4369221ca6b34dcc56bf57ba51d4e86fc
Shubh <patelshubh694@gmail.com>
2021-09-25 15:17:15 +0530

    Added Map and Location Pointer with Popup

--- airbnb-clone/next.config.js
+++ airbnb-clone/next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ["upload.wikimedia.org", "a0.muscache.com", "links.papareact.com"]
+    },
+    env: {
+        mapbox_key: "pk.eyJ1Ijoic2h1YmgtMTgiLCJhIjoiY2t0emlqazFlMHF2NzJ0b3E5ZGlqbGJ0eiJ9.d0hyRWfI6iXIUPT0ig1ryw"
     }
-}
+};
